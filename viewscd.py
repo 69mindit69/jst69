7f5af84ef8516f2c775f02c9ab7f6f9c0fd2f238
69mindit69 <160328655+69mindit69@users.noreply.github.com>
2024-07-10 15:03:20 +0530

    Delete viewscd.py

--- viewscd.py
+++
@@ -1,8 +0,0 @@
-views.py
-from django.shortcuts import render
-import datetime
-from django.http import HttpResponse
-# Create your views here.
-dt=datetime.datetime.now()
-resp="<h1>Current Date and Time is %s<h1>"%(dt)
-return HttpResponse(resp)
