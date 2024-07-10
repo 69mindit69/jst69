7a6353a200c6bd4360554f2e8be60e11e43c54ec
69mindit69 <160328655+69mindit69@users.noreply.github.com>
2024-07-10 15:02:42 +0530

    Delete viewa_layot.py

--- viewa_layot.py
+++
@@ -1,9 +0,0 @@
-views.py
-from django.shortcuts import render
-# Create your views here.
-def home(request):
-return render(request,'home.html')
-def contactus(request):
-return render(request,'contactus.html')
-def aboutus(request):
-return render(request,'about.html')
