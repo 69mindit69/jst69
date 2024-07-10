c08cd589cf80f33a5c514e4e41c4f45c42ca410e
69mindit69 <160328655+69mindit69@users.noreply.github.com>
2024-07-10 15:02:55 +0530

    Delete views_student.py

--- views_student.py
+++
@@ -1,8 +0,0 @@
-Views.py
-from django.shortcuts import render
-# Create your views here.
-def fruit_student(request):
-fruitList=['Mango','Kiwi','Banana','Apple','Grapes']
-studentList=['Rama','Chetan','Kumar','Harish','Geetha']
-return 
-render(request,'fruit_student.html',{'fruitList':fruitList,'studentList':sorted(studentList)})
