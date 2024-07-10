487ce0b9e0b6343725c79cce844f3e1ee1b99762
69mindit69 <160328655+69mindit69@users.noreply.github.com>
2024-07-10 15:02:13 +0530

    Delete urls_student.py

--- urls_student.py
+++
@@ -1,8 +0,0 @@
-urls.py
-from django.contrib import admin
-from django.urls import path
-from FruitsApp.views import fruit_student
-urlpatterns = [
-path('admin/', admin.site.urls),
-path('fruits/',fruit_student),
-]
